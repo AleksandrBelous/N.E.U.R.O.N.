74a991548b2169f5f19d26ba69d22ec16f220a95
AleksandrBelous <ab21082001@mail.ru>
2024-04-14 07:29:16 +0300

    smth

--- DataPrep.py
+++ DataPrep.py
@@ -8,33 +8,56 @@ from scapy.all import sniff, rdpcap, wrpcap
 class DataPrep:
 
     def __init__(self):
-        self.packets_list = list()
+        self.pkts_list = list()
         self.filter = "ip and tcp"
-        self.num_packets_to_sniff = 10
+        self.num_pkts_to_sniff = 10
         self.last_time = None
         self.count = 0
         self.row_data = list()
         self.pcap_to_read = ''
-        # self.num_packets_to_create = 50000
         self.is_norm_sample = ''
 
     def set_default(self):
-        self.packets_list = list()
+        """
+        set attributes to default values
+        :return: None
+        """
+        self.pkts_list = list()
         self.last_time = None
         self.row_data = list()
         self.pcap_to_read = ''
 
-    def sniffing_packets(self, packet):
-        self.packets_list.append(packet)
+    def sniffing_packets(self, pkt):
+        """
+        help function for packets sniffing
+        :param pkt: yet sniffed packet
+        :return: None, put sniffed packets at 'pkts_list' attribute
+        """
+        self.pkts_list.append(pkt)
 
-    def analyze_10_packets(self, packets):
+    def analyze_10_packets(self, pkts):
+        """
+        analyze 10 packets to get target network characteristics
+        :param pkts: 10 target packets, TCP/IP only !!!
+        :return:
+        - "Average Packet Size", the average length of the TCP/IP packet data field (hereinafter referred to as the packet length)
+        - "Flow Bytes/s", the data flow rate
+        - "Max Packet Length", the maximum packet length
+        - "Fwd Packet Length Mean", the average length of packets transmitted in the forward direction
+        - "Fwd IAT Min", the minimum value of the inter-packet interval (IAT, inter-arrival time) in the forward direction
+        - "Total Length of Fwd Packets", the total length of packets transmitted in the forward direction
+        - "Fwd IAT Std", the standard deviation of the value of the inter-packet interval in the forward direction of the packets
+        - "Flow IAT Mean", the average value of the batch interval
+        - "Fwd Packet Length Max", the maximum length of a packet transmitted in the forward direction
+        - "Fwd Header Length", the total length of the headers of packets transmitted in the forward direction
+        """
         total_length = 0
         max_pkt_len = 0
         fwd_pkts_len = []
         fwd_ia_times = []
         fwd_header_len = 0
 
-        for packet in packets:
+        for packet in pkts:
             try:
                 total_length += len(packet)
                 max_pkt_len = max(max_pkt_len, len(packet))
@@ -49,16 +72,14 @@ class DataPrep:
                 print(f"can't analyze packet:")
                 packet.show()
         try:
-            avg_pkt_size = float(round(total_length / len(packets), 3))
-            flow_bytes_per_s = float(round(total_length / (packets[-1].time - packets[0].time), 3))
+            avg_pkt_size = float(round(total_length / len(pkts), 3))
+            flow_bytes_per_s = float(round(total_length / (pkts[-1].time - pkts[0].time), 3))
             fwd_pkt_mean_len = float(round(sum(fwd_pkts_len) / len(fwd_pkts_len), 3))
             fwd_iat_min = float(round(min(fwd_ia_times) * 1000, 5))
             tot_len_fwd_pkts = sum(fwd_pkts_len)
             fwd_iat_std = float(round(stdev(fwd_ia_times), 5))
             flow_iat_mean = float(round(sum(fwd_ia_times) / len(fwd_ia_times), 5))
             fwd_pkt_max_len = max(fwd_pkts_len)
-
-            # Вывод результатов
             # print(len(fwd_ia_times), fwd_ia_times)
             # print("Average Packet Size:", avg_pkt_size)
             # print("Flow Bytes/s:", flow_bytes_per_s)
@@ -86,26 +107,45 @@ class DataPrep:
             print(e)
 
     def analyze_pcap(self, pkts):
+        """
+        group and analyze packets by 10
+        :param pkts: target list with packets
+        :return: None, put unlabeled data samples at 'row_data' attribute
+        """
         n = len(pkts) + 1
         i, j = 0, 10
         while i + 10 < n:
-            # print()
             self.row_data.append(self.analyze_10_packets(pkts[i:j]))
             i, j = j, j + 10
-            # print()
-        # print(*self.row_data, sep='\n')
 
     def start_sniffing(self):
-        sniff(prn=self.sniffing_packets, filter=self.filter, count=self.num_packets_to_sniff)
+        """
+        start sniffing with given params
+        :return: None, put sniffed packets at 'pkts_list' attribute
+        """
+        sniff(prn=self.sniffing_packets, filter=self.filter, count=self.num_pkts_to_sniff)
 
     def read_pcap(self):
-        self.packets_list = rdpcap(self.pcap_to_read)
+        """
+        put the .pcap file at 'pkts_list' attribute
+        :return: None, put the .pcap file at 'pkts_list' attribute
+        """
+        self.pkts_list = rdpcap(self.pcap_to_read)
 
-    def create_pcap(self, num_packets_to_create):
+    def create_pcap_from_net(self, num_packets_to_create):
+        """
+        start sniffing the network and store packets at local .pcap file
+        :param num_packets_to_create: number of packets to sniff
+        :return: None, create 'my{num_packets_to_create}.pcap' file at local folder
+        """
         sniff(prn=self.sniffing_packets, filter=self.filter, count=num_packets_to_create)
-        wrpcap(f'my{num_packets_to_create}.pcap', self.packets_list)
+        wrpcap(f'my{num_packets_to_create}.pcap', self.pkts_list)
 
     def create_csv(self):
+        """
+        create local 'csvs/' folder with .csv datasets based on 'pcap_to_read' attribute
+        :return: None, create local 'csvs/' folder
+        """
         file_name, file_extension = os.path.splitext(self.pcap_to_read)
         print(file_name, file_extension)
         new_folder = os.path.join(os.getcwd(), 'csvs')
@@ -118,22 +158,21 @@ class DataPrep:
                 f.write(','.join([str(e) for e in tpl] + [self.is_norm_sample]))
 
     def set_samples(self, target_folder: str, y_correct: int):
+        """
+        read the prepared .pcap file and convert it to datasets
+        :param target_folder: folder with .pcap[s] file[s]
+        :param y_correct: 0 for normal traffic, 1 for malicious
+        :return: create 'csvs' folder with .csv datasets
+        """
         self.is_norm_sample = str(y_correct) + '\n'
         attacks_pcaps_folder = os.path.join(os.getcwd(), target_folder)
-        print('att >>', attacks_pcaps_folder)
         for file in os.listdir(attacks_pcaps_folder):
-            print('file is ', file)
             self.pcap_to_read = os.path.join(attacks_pcaps_folder, file)
             self.read_pcap()
-            self.analyze_pcap(sn.packets_list)
+            self.analyze_pcap(self.pkts_list)
             self.create_csv()
             self.set_default()
 
 
 if __name__ == '__main__':
-    sn = DataPrep()
-    # sn.start_sniffing()
-    # sn.analyze_packets(sn.packets_list)
-    # sn.set_default()
-    sn.set_samples('my_pcaps', 0)
-    # sn.create_pcap(50000)
+    ...
